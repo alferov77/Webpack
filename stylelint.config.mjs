fc920e0700983096ffa94176ab6282ba82926f44
Andrej Alferov <alferov77@gmail.com>
2024-09-15 12:11:44 +0200

    first commit

---
+++ stylelint.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  extends: 'stylelint-config-standard',
+  rules: {
+    'no-empty-source': null,
+  },
+};
+
+
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new TerserPlugin,
+    new CssMinimizerPlugin(),
+    new ESLintPlugin({
+      overrideConfigFile: path.resolve(__dirname, 'eslint.config.mjs'),
+      context: path.resolve(__dirname, 'src'),
+      extensions: ['js', 'mjs'],
+    }),
+    new StylelintPlugin({
+      overrideConfigFile: path.resolve(__dirname, 'stylelint.config.mjs'),
+      context: path.resolve(__dirname, 'src'),
+      extensions: ['css', 'scss'],
+    }),
+    new HtmlWebpackPlugin( {
+      template: './src/index.pug',
+      filename: 'index.html'
+    })
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            esModule: true,
+          }
+        }, 'css-loader'],
+      },
+      {
+        test: /\.pug$/,
+        use: 'pug-loader'
+      },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader'
+      }
+    ]
+
+  },
+};
